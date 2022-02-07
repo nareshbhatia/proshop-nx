d9a081b27b8003e666cd0c723ba9cb7c3552f88e
Naresh Bhatia <naresh.archfirst@gmail.com>
2022-02-06 20:06:43 -0500

    Delete default content from main page

--- apps/catalog/pages/_app.tsx
+++ apps/catalog/pages/_app.tsx
@@ -8,7 +8,7 @@ function CustomApp({ Component, pageProps }: AppProps) {
       <Head>
         <title>Welcome to catalog!</title>
       </Head>
-      <main className="app">
+      <main>
         <Component {...pageProps} />
       </main>
     </>
