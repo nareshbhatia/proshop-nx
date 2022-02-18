8b8d657b7463d5e1f20f462230a6957ea0d6ef35
Naresh Bhatia <naresh.archfirst@gmail.com>
2022-02-18 03:19:57 -0500

    Implemented "Place your Order" button in the cart.
    It now makes the appropriate GraphQL call.

--- apps/cart/components/CartView/PlaceOrderPanel.tsx
+++ apps/cart/components/CartView/PlaceOrderPanel.tsx
@@ -1,18 +1,48 @@
 import * as React from 'react';
+import { gql, useMutation } from '@apollo/client';
 import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
+import Snackbar from '@mui/material/Snackbar';
 import Typography from '@mui/material/Typography';
 import { Cart } from '@proshop-nx/domain';
 import { NumberUtils } from '@react-force/number-utils';
+
+const PlaceOrderFromCart = gql`
+  mutation PlaceOrderFromCart {
+    placeOrderFromCart {
+      id
+      createdAt
+    }
+  }
+`;
 
 export interface PlaceOrderPanelProps {
   cart: Cart;
 }
 
 export function PlaceOrderPanel({ cart }: PlaceOrderPanelProps) {
+  const [placeOrder, { loading }] = useMutation(PlaceOrderFromCart);
+  const [snackbarOpen, setSnackbarOpen] = React.useState(false);
+
   const { totalQuantity, totalPrice } = cart;
   const fmtQty = totalQuantity.toString();
   const fmtPrice = NumberUtils.formatAsMoney(totalPrice);
+
+  const handlePlaceOrder = async () => {
+    await placeOrder();
+    setSnackbarOpen(true);
+  };
+
+  const handleSnackbarClose = (
+    event: React.SyntheticEvent | Event,
+    reason?: string
+  ) => {
+    if (reason === 'clickaway') {
+      return;
+    }
+
+    setSnackbarOpen(false);
+  };
 
   return (
     <React.Fragment>
@@ -25,9 +55,22 @@ export function PlaceOrderPanel({ cart }: PlaceOrderPanelProps) {
           ${fmtPrice}
         </Typography>
       </Box>
-      <Button variant="contained" color="secondary" sx={{ mt: 1 }}>
+      <Button
+        variant="contained"
+        color="secondary"
+        sx={{ mt: 1 }}
+        disabled={loading}
+        onClick={handlePlaceOrder}
+      >
         Place your order ({fmtQty} {totalQuantity === 1 ? 'item' : 'items'})
       </Button>
+      <Snackbar
+        anchorOrigin={{ vertical: 'bottom', horizontal: 'center' }}
+        autoHideDuration={6000}
+        open={snackbarOpen}
+        onClose={handleSnackbarClose}
+        message="Order placed"
+      />
     </React.Fragment>
   );
 }
